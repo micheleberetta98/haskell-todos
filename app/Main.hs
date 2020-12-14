9c2fe2a23e20c6cf3631aab97fddfa5818bededc
Michele Beretta <beretta.m98@gmail.com>
2020-12-14 19:09:34 +0100

    better command handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,10 @@
 module Main where
 
-import           Control.Monad.State (runState)
-import           System.Environment
-import qualified TodoList            as TL
-import           TodoState           (ListAction (..), TodoState,
-                                      UpdateAction (..), list, update)
-
-import           Record              (readFromFile, writeToFile)
-
-data Command = Add String
-  | Do Int
-  | Undo Int
-  | Invalid String
-  | ListAll
-  | ListDone
-  | ListNotDone
-  | Quit
+import           Command       (CommandResult (..), execute, isQuit, parse)
+import           Control.Monad
+import           Record        (readFromFile, writeToFile)
+import           System.IO
+import           TodoList      (TodoList, count)
 
 main :: IO ()
 main = do
@@ -34,50 +23,30 @@ main = do
   -- let filename = head args
   let filename = "test.txt"
   ts <- readFromFile filename
+  (saveContent, ts') <- loop ts
+  when saveContent $ writeToFile filename ts'
 
-  loop filename ts
+loop :: TodoList -> IO (Bool, TodoList)
+loop s = do
+  line <- prompt "> "
+  let
+    cmd = parse line
+    result = execute cmd s
+  printResult result
+  case result of
+    OkContinue (_, s') -> loop s'
+    OkQuit _           -> return (True, s)
+    NotOk _            -> loop s
 
-loop :: FilePath -> TL.TodoList -> IO ()
-loop filename s = do
-  putStr " > "
-  line <- getLine
-  let cmd = parse line
-  result <- execute cmd s filename
-  mapM_ (loop filename) result
+prompt :: String -> IO String
+prompt s = do
+  putStr s
+  hFlush stdout
+  getLine
 
-parse :: String -> Command
-parse ('+':' ':name) = Add name
-parse ('d':' ':int)  = Do (read int)
-parse ('u':' ':int)  = Undo (read int)
-parse ('l':'s':_)    = ListAll
-parse ('l':'d':_)    = ListDone
-parse ('l':'n':_)    = ListNotDone
-parse ('q':_)        = Quit
-parse s              = Invalid s
-
-execute :: Command -> TL.TodoList -> FilePath -> IO (Maybe TL.TodoList)
-execute (Invalid line) s _ = do
-  putStrLn $ "Invalid command: `" ++ line ++ "`"
-  return (Just s)
-execute Quit s filename = do
-  putStrLn "Bye!"
-  writeToFile filename s
-  return Nothing
-execute cmd s _ = do
-  let (tl, s') = runState (apply cmd) s
-  printResult tl
-  return (Just s')
-
-printResult :: TL.TodoList -> IO ()
-printResult tl
-  | TL.count tl == 0 = return ()
-  | otherwise        = print tl
-
-apply :: Command -> TodoState TL.TodoList
-apply (Add name)  = update (AddTodo name) >> return TL.empty
-apply (Do i)      = update (SetTodoDone i) >> return TL.empty
-apply (Undo i)    = update (UnsetTodoDone i) >> return TL.empty
-apply ListAll     = list ListAllTodos
-apply ListDone    = list ListDoneTodos
-apply ListNotDone = list ListNotDoneTodos
-apply Quit        = return TL.empty
+printResult :: CommandResult -> IO ()
+printResult (OkQuit msg) = putStrLn msg
+printResult (NotOk msg) = putStrLn msg
+printResult (OkContinue (tl, _))
+  | count tl == 0 = return ()
+  | otherwise     = print tl
---
+++ src/Command.hs
@@ -0,0 +1,64 @@
+module Command (Command, CommandResult(..), parse, execute, isQuit, isInvalid) where
+
+import           Control.Monad.State (runState)
+import           TodoList            (TodoList, empty)
+import           TodoState           (ListAction (..), TodoState,
+                                      UpdateAction (..), list, update)
+
+----------------------- DATA
+data Command =
+  Add String
+  | Do Int
+  | Undo Int
+  | ListAll
+  | ListDone
+  | ListNotDone
+  | Quit
+  | Invalid String
+  deriving (Eq)
+
+data CommandResult = OkContinue (TodoList, TodoList) | OkQuit String | NotOk String
+
+----------------------- FUNCTIONS
+parse :: String -> Command
+parse = uncurry parse' . split
+  where
+    parse' "+"    (Just name) = Add name
+    parse' "do"   (Just int)  = Do (read int)
+    parse' "undo" (Just int)  = Undo (read int)
+    parse' "ls"   _           = ListAll
+    parse' "ld"   _           = ListDone
+    parse' "ln"   _           = ListNotDone
+    parse' "q"    _           = Quit
+    parse' cmd    _           = Invalid cmd
+
+split :: String -> (String, Maybe String)
+split s = (cmd, arg)
+  where
+    (cmd, rest) = break (==' ') s
+    arg = tail' rest
+
+tail' :: String -> Maybe String
+tail' "" = Nothing
+tail' s  = Just $ tail s
+
+isQuit :: Command -> Bool
+isQuit = (==Quit)
+
+isInvalid :: Command -> Bool
+isInvalid (Invalid _) = True
+isInvalid _           = False
+
+execute :: Command -> TodoList -> CommandResult
+execute Quit        _ = OkQuit "Bye!"
+execute (Invalid s) _ = NotOk $ "Invalid command `" ++ s ++ "`"
+execute cmd         s = OkContinue $ runState (apply cmd) s
+
+apply :: Command -> TodoState TodoList
+apply (Add name)  = update (AddTodo name) >> return empty
+apply (Do i)      = update (SetTodoDone i) >> return empty
+apply (Undo i)    = update (UnsetTodoDone i) >> return empty
+apply ListAll     = list ListAllTodos
+apply ListDone    = list ListDoneTodos
+apply ListNotDone = list ListNotDoneTodos
+apply Quit        = return empty

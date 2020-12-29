6f3234d205787b8cc48b6b8c9727acf3b16bdb59
Michele Beretta <beretta.m98@gmail.com>
2020-12-29 19:13:15 +0100

    now using Data.Map instead of a list to keep the todos

--- src/TodoList.hs
+++ src/TodoList.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module TodoList
   ( TodoList
   , empty
@@ -9,38 +11,45 @@ module TodoList
   , filter'
 ) where
 
-import           Prettify (Pretty (..))
-import           Record (FromRecord (..), ToRecord (..))
-import           Todo   (Todo, newTodo, setDone, setNotDone)
+import           Data.Bifunctor (bimap, second)
+import qualified Data.Map       as M
+import           Prettify       (Pretty (..))
+import           Record         (FromRecord (..), ToRecord (..))
+import           Todo           (Todo, newTodo, setDone, setNotDone)
 
 ----------------------- DATA
-newtype TodoList = TodoList [Todo] deriving (Show, Eq)
+type TodoList = M.Map Int Todo
 
 ----------------------- INSTANCES
 instance Pretty TodoList where
-  prettify (TodoList tl) = unlines $ zipWith prettify' [0..] tl
-    where prettify' i t = show i ++ ". " ++ prettify t
+  prettify = unlines . map prettify' . M.assocs
+    where prettify' (i, t) = show i ++ ". " ++ prettify t
 
 instance ToRecord TodoList where
-  toRecord (TodoList tl) = unlines $ map toRecord tl
+  toRecord = unlines . map toRecord' . M.assocs
+    where
+      toRecord' (i, t) = show i ++ ";" ++ toRecord t
 
 instance FromRecord TodoList where
-  fromRecord = TodoList . map fromRecord . filter (/="") . lines
+  fromRecord = M.fromList . map fromRecord' . filter (/="") . lines
+    where
+      fromRecord' = second fromRecord . parts
+
+      parts :: String -> (Int, String)
+      parts s = bimap read tail $ break (==';') s
 
 ----------------------- FUNCTIONS
 empty :: TodoList
-empty = TodoList []
+empty = M.empty
 
 count :: TodoList -> Int
-count (TodoList tl) = length tl
+count = M.size
 
 get :: Int -> TodoList -> Maybe Todo
-get i (TodoList tl)
-  | i >= length tl = Nothing
-  | otherwise     = Just $ tl !! i
+get = M.lookup
 
 add :: String -> TodoList -> TodoList
-add n (TodoList tl) = TodoList (newTodo n : tl)
+add n tl = M.insert (count tl) (newTodo n) tl
 
 doTodo :: Int -> TodoList -> TodoList
 doTodo = applyToTodo setDone
@@ -49,11 +58,7 @@ undoTodo :: Int -> TodoList -> TodoList
 undoTodo = applyToTodo setNotDone
 
 applyToTodo :: (Todo -> Todo) -> Int -> TodoList -> TodoList
-applyToTodo f i (TodoList tl) = TodoList $ zipWith changeState [0..] tl
-  where
-    changeState j
-      | j == i    = f
-      | otherwise = id
+applyToTodo = M.adjust
 
 filter' :: (Todo -> Bool) -> TodoList -> TodoList
-filter' f (TodoList tl) = TodoList (filter f tl)
+filter' = M.filter

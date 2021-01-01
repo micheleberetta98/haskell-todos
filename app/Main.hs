dda7690cf71f22c2c4895535845305a4959b0700
Michele Beretta <beretta.m98@gmail.com>
2021-01-01 18:09:05 +0100

    added cli arguments validation

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,38 @@
 module Main where
 
 import           Command            (CommandResult (..), execute, parse)
+import           Control.Monad      (unless)
 import           Data.Maybe         (fromMaybe)
 import           Prettify           (prettify)
 import           Record             (readFromFile, writeToFile)
+import           System.Directory   (doesFileExist)
 import           System.Environment (getArgs)
 import           System.IO          (hFlush, stdout)
 import           TodoList           (TodoList, count, empty)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn "==================================="
-  putStrLn "Command:"
-  putStrLn "  + [content]    Add new todo"
-  putStrLn "  do [i]         Set todo done"
-  putStrLn "  undo [i]       Set todo undone"
-  putStrLn "  ls             List all todos"
-  putStrLn "  ld             List done todos"
-  putStrLn "  ln             List not done todos"
-  putStrLn "  q              Quit"
-  putStrLn "==================================="
+  fname <- getArgs >>= validateArgs
+  unless (null fname) $ do
+    putStrLn "==================================="
+    putStrLn "Command:"
+    putStrLn "  + [content]    Add new todo"
+    putStrLn "  do [i]         Set todo done"
+    putStrLn "  undo [i]       Set todo undone"
+    putStrLn "  ls             List all todos"
+    putStrLn "  ld             List done todos"
+    putStrLn "  ln             List not done todos"
+    putStrLn "  q              Quit"
+    putStrLn "==================================="
+    readFromFile fname >>= loop . fromMaybe empty >>= writeToFile fname
 
-  let filename = head args
-  readFromFile filename >>= loop . fromMaybe empty >>= writeToFile filename
+validateArgs :: [String] -> IO FilePath
+validateArgs []        = putStrLn "Missing filename (first argument)" >> return ""
+validateArgs (fname:_) = do
+  ok <- doesFileExist fname
+  if ok
+    then return fname
+    else putStrLn ("No such file: `" ++ fname ++ "`") >> return ""
 
 loop :: TodoList -> IO TodoList
 loop s = do
@@ -40,7 +49,7 @@ prompt s = putStr s >> hFlush stdout >> getLine
 
 printResult :: CommandResult -> IO ()
 printResult (OkQuit msg) = putStrLn msg
-printResult (NotOk msg) = putStrLn msg
+printResult (NotOk msg)  = putStrLn msg
 printResult (OkContinue (tl, _))
-  | count tl == 0 = return ()
-  | otherwise     = putStrLn (prettify tl)
+  | count tl == 0        = return ()
+  | otherwise            = putStrLn (prettify tl)
